3dced080bb994d0fe2a03d90ddfa6c38b1cce1bb
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-07-12 15:19:58 +0300

    Initial commit

---
+++ src/com/learnprogramming/Main.java
@@ -0,0 +1,39 @@
+package com.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
+
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was " + finalScore);
+
+        }
+
+
+//
+//        if (score < 1000 && score > 1000) {
+//            System.out.println("Your score was less than 5000 but greater than 1000");
+//        } else if (score < 1000) {
+//            System.out.println("Your score was less than 1000");
+//        } else {
+//            System.out.println("Got here.");
+//        }
+    }
+}
