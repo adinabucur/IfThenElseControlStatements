d6501f910adcec4a81c9eaa6ab3533c6a23b40a5
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-07-12 18:15:35 +0300

    Commit
    -avoiding many return statements

--- src/com/learnprogramming/Main.java
+++ src/com/learnprogramming/Main.java
@@ -8,25 +8,50 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Daniel", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Adrian", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gabriela", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Gilbert", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Carol", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Louise", highScorePosition);
+
     }
 
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
-
+            return finalScore;
         }
 
-
+        return -1;
 //
 //        if (score < 1000 && score > 1000) {
 //            System.out.println("Your score was less than 5000 but greater than 1000");
@@ -35,5 +60,32 @@ public class Main {
 //        } else {
 //            System.out.println("Got here.");
 //        }
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " manged to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+//        if (score >= 1000) {
+//            return 1;
+//        } else if (score >= 500) {
+//            return 2;
+//        } else if (score >= 100) {
+//            return 3;
+//        }
+//        return 4;
+
+        int position = 4; //assuming position 4 will be returned;
+        if (score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position = 2;
+        } else if (score >= 100) {
+            position = 3;
+        }
+        return position;
+
+
     }
 }
